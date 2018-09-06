eb3b68ce53cc95c686cc6abaef554b4d03eea1ef
Siarhei Hauruseu <sig@enonic.com>
2018-09-06 13:11:16 +0300

    test for deleting users

---
+++ src/main/k6/delete_user_test.js
@@ -0,0 +1,57 @@
+import {group, check, sleep} from "k6";
+import {Trend} from "k6/metrics";
+import * as common from "./common.js";
+
+export let options = {
+    stages: [
+        {duration: "5s", target: "5"},
+        {duration: "15s", target: "5"},
+        {duration: "5s", target: "0"}
+    ],
+    thresholds: {
+        "user_delete": ["avg<200"],
+        "failed requests": ["rate<0.1"],
+        "http_req_duration": ["p(95)<10000", "avg<5000"],
+        "http_req_connecting": ["max<3"]
+    },
+    ext: {
+        loadimpact: {
+            projectID: 3114611,
+            name: "K6 deleting of users",
+            distribution: {
+                scenarioLabel1: {loadZone: "amazon:kr:seoul", percent: 50},
+                scenarioLabel2: {loadZone: "amazon:ie:dublin", percent: 50}
+            }
+        }
+    }
+};
+const baseUrl = 'http://127.0.0.1:8080/admin';
+const restUrl = 'http://127.0.0.1:8080/admin/rest';
+const deleteUserMetric = new Trend("user_delete");
+
+export default function () {
+    common.xp_login("su", "password", restUrl, true);
+    group("user_delete", function () {
+
+        let userName = 'user-' + Math.floor((Math.random() * 1000000000) + 1);
+        let email = userName + '@gmail.com';
+        let res1 = common.createUser(userName, email, 'password', baseUrl);
+        sleep(5);
+        if (res1.status === 200) {
+            console.log("new User has been created!, timings.duration is :" + res1.timings.duration);
+        };
+        console.log("User should be deleted :" + userName);
+        let respDelete = common.deleteUser(baseUrl,userName);
+        check(respDelete, {
+            "status is 200": (respDelete) => {
+                respDelete.status === 200
+            },
+            "content-type is application/json": (respDelete) => respDelete.headers['Content-Type'] === "application/json",
+            "transaction time OK": (respDelete) => {
+                respDelete.timings.duration < 200;
+            }
+        });
+        deleteUserMetric.add(respDelete.timings.duration);
+        sleep(10);
+    })
+};
